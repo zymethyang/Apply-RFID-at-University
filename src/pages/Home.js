d9f23c72f42c56350bf6b85be9e2b285cdb0f478
Tri Nguyen <zymethyang@gmail.com>
2018-04-21 14:58:40 +0700

    finished ui/ux

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,87 @@
 import React, { Component } from 'react';
-
+import callApi from '../utils/callApi';
+import mqtt from 'mqtt';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listStudent: [],
+            search: "",
+            realtime: []
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        callApi('student').then(students => {
+            this.setState({
+                listStudent: students.data
+            })
+        });
+    }
+
+    filterStr = (Arr, mStr) => {
+        let result = [];
+        Arr.forEach((value, index) => {
+            if (value.ten.toLowerCase().indexOf(mStr.toLowerCase()) !== -1) {
+                result.push(value);
+            }
+        });
+        return result;
+    }
+
+    handleChange = (event) => {
+        this.setState({ search: event.target.value });
+    }
+
+    mqttRealtime = (tmpRealtime) => {
+        var client = mqtt.connect({
+            protocol: 'wss',
+            host: 'm13.cloudmqtt.com',
+            port: '34250',
+            password: 'B9Io8J5H88fP',
+            username: 'jepjknnb'
+        });
+        client.on('connect', function () {
+            client.subscribe('student')
+        })
+        client.on('message', function (topic, message) {
+            tmpRealtime.push(JSON.parse(message.toString()));
+            this.setState({
+                realtime: tmpRealtime
+            });
+            client.end();
+        });
+    }
+
     render() {
+        this.mqttRealtime(this.state.realtime);
+        let stu = this.filterStr(this.state.listStudent, this.state.search);
         return (
             <div className="row">
                 <div className="col l6" style={{ marginTop: 20 }}>
-                    <div class="collection">
-                        <a class="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Ngay Quet The</a>
-                        <a class="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Ngay Quet The</a>
-                        <a class="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Ngay Quet The</a>
-                        <a class="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Ngay Quet The</a>
+                    <div className="collection">
+                        <a className="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Ngay Quet The</a>
+                        <a className="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Ngay Quet The</a>
+                        <a className="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Ngay Quet The</a>
+                        <a className="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Ngay Quet The</a>
                     </div>
                 </div>
                 <div className="col l6" style={{ marginTop: 20 }}>
-                    <div class="card">
-                        <div class="card-content">
+                    <div className="card">
+                        <div className="card-content">
                             <span className="row">
                                 <span className="col l10">
-                                    <input value="Alvin" type="text" class="validate" />
+                                    <input value={this.state.search} type="text" className="validate" onChange={this.handleChange} />
                                 </span>
                                 <span className="col l2">
-                                    <i class="material-icons" style={{ fontSize: 50 }}>search</i>
+                                    <i className="material-icons" style={{ fontSize: 50 }}>search</i>
                                 </span>
                             </span>
                             <span className="row">
-                                <div class="collection">
-                                    <a class="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Su kien tham gia - Diem ren luyen</a>
-                                    <a class="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Su kien tham gia - Diem ren luyen</a>
-                                    <a class="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Su kien tham gia - Diem ren luyen</a>
-                                    <a class="collection-item" style={{ color: "#810173", fontWeight: 'bold' }}>Ten - Su kien tham gia - Diem ren luyen</a>
+                                <div className="collection">
+                                    {this.renderListStudent(stu)}
                                 </div>
                             </span>
                         </div>
@@ -37,6 +89,25 @@ class Home extends Component {
                 </div>
             </div>
         );
+    }
+
+    renderListStudent = (stu) => {
+        let result = null;
+        result = stu.map((value, index) => {
+            let sktg = "";
+            if (value.sktg.length === 0) {
+                sktg = "Chưa tham gia!";
+            } else {
+                sktg = "";
+                value.sktg.forEach(value => {
+                    sktg = sktg + " " + value;
+                });
+            }
+            return (
+                <a className="collection-item" style={{ color: "#810173", fontWeight: 'bold' }} key={index}>{value.ten} - {sktg} - {value.diem} Điểm</a>
+            );
+        })
+        return result;
     }
 }
 
--- src/pages/ListEvents.js
+++ src/pages/ListEvents.js
@@ -1,80 +1,160 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import callApi from '../utils/callApi';
+
 
 class ListEvents extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tensk: "",
+            diemCong: 0,
+            tgbd: "",
+            gbd: "",
+            tgkt: "",
+            gkt: "",
+            dataEvents: []
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    componentDidMount() {
+        callApi('event').then(res => {
+            this.setState({
+                dataEvents: res.data
+            });
+            console.log(res.data);
+        });
+    }
+
+    renderListEvents = (dataEvents) => {
+        let result = null;
+        result = dataEvents.map((value, index) => {
+            return (
+                <tr key={index}>
+                    <td>{value.ten}</td>
+                    <td>{moment.unix(value.tgbd).format("DD/MM/YYYY hh:mm A")}</td>
+                    <td>{moment.unix(value.tgkt).format("DD/MM/YYYY hh:mm A")}</td>
+                    <td>{value.diemCong}</td>
+                    <td>{value.thDu}</td>
+                </tr>
+            );
+        })
+        return result;
+    }
+
     render() {
+        let { dataEvents } = this.state;
         return (
             <div className="row">
                 <div className="col l8" style={{ marginTop: 10 }}>
-                    <div class="card" style={{ backgroundColor: '#810173' }}>
-                        <div class="card-content white-text">
+                    <div className="card" style={{ backgroundColor: 'white' }}>
+                        <div className="card-content black-text">
                             <table>
                                 <thead>
                                     <tr>
                                         <th>Tên sự kiện</th>
                                         <th>Thời gian bắt đầu</th>
                                         <th>Thời gian kết thúc</th>
-                                        <th>Numbers Student</th>
+                                        <th>Điểm cộng</th>
+                                        <th>Số lượng tham dự</th>
                                     </tr>
                                 </thead>
 
                                 <tbody>
-                                    <tr>
-                                        <td>Alvin</td>
-                                        <td>Eclair</td>
-                                        <td>$0.87</td>
-                                        <td>$0.87</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Alan</td>
-                                        <td>Jellybean</td>
-                                        <td>$3.76</td>
-                                        <td>$3.76</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Jonathan</td>
-                                        <td>Lollipop</td>
-                                        <td>$7.00</td>
-                                        <td>$7.00</td>
-                                    </tr>
+                                    {this.renderListEvents(dataEvents)}
                                 </tbody>
                             </table>
                         </div>
                     </div>
                 </div>
                 <div className="col l4" style={{ marginTop: 10 }}>
-                    <div class="card" style={{ backgroundColor: '#09158A' }}>
-                        <div class="card-content white-text">
+                    <div className="card" style={{ backgroundColor: '#white' }}>
+                        <div className="card-content black-text">
                             <span style={{ fontWeight: 'bold', fontSize: 24 }}>
-                                Them su kien
-                            </span>
+                                Thêm sự kiện
+                                </span>
                             <div className="row">
-                                <div class="input-field" style={{ paddingLeft: 15, paddingRight: 15 }}>
-                                    <input placeholder="Nhap Ten Su kien" id="name" type="text" class="validate" style={{ color: 'white', fontWeight: 'bolder' }} />
+                                <div className="input-field" style={{ paddingLeft: 15, paddingRight: 15 }}>
+                                    <input placeholder="Nhập tên sự kiện" name="tensk" type="text" className="validate" style={{ color: 'black', fontWeight: 'bolder' }} value={this.state.tensk}
+                                        onChange={this.handleInputChange} />
                                 </div>
                             </div>
                             <div className="row">
-                                <div class="input-field" style={{ paddingLeft: 15, paddingRight: 15 }}>
-                                    <input placeholder="Nhap Diem Cong" id="name" type="text" class="validate" style={{ color: 'white', fontWeight: 'bolder' }} />
+                                <div className="input-field" style={{ paddingLeft: 15, paddingRight: 15 }}>
+                                    <input placeholder="Nhập điểm cộng" name="diemCong" type="number" className="validate" style={{ color: 'black', fontWeight: 'bolder' }} value={this.state.diemCong}
+                                        onChange={this.handleInputChange} />
                                 </div>
                             </div>
                             <div className="row">
-                                <div class="input-field" style={{ paddingLeft: 15, paddingRight: 15 }}>
-                                    <input type="date" name="bday" min="2000-01-02" style={{ color: 'white' }} />
+                                <div className="col l7">
+                                    <div className="input-field" style={{ paddingLeft: 15, paddingRight: 15 }}>
+                                        <input type="date" name="tgbd" min="2000-01-02" style={{ color: 'black' }} value={this.state.tgbd}
+                                            onChange={this.handleInputChange} />
+                                    </div>
+                                </div>
+                                <div className="col l5">
+                                    <div className="input-field" style={{ paddingLeft: 15, paddingRight: 15 }}>
+                                        <input type="time" name="gbd" style={{ color: 'black' }} value={this.state.gbd}
+                                            onChange={this.handleInputChange} />
+                                    </div>
                                 </div>
                             </div>
                             <div className="row">
-                                <div class="input-field" style={{ paddingLeft: 15, paddingRight: 15 }}>
-                                    <input type="date" name="bday" min="2000-01-02" style={{ color: 'white' }} />
+                                <div className="col l7">
+                                    <div className="input-field" style={{ paddingLeft: 15, paddingRight: 15 }}>
+                                        <input type="date" name="tgkt" min="2000-01-02" style={{ color: 'black' }} value={this.state.tgkt}
+                                            onChange={this.handleInputChange} />
+                                    </div>
+                                </div>
+                                <div className="col l5">
+                                    <div className="input-field" style={{ paddingLeft: 15, paddingRight: 15 }}>
+                                        <input type="time" name="gkt" style={{ color: 'black' }} value={this.state.gkt}
+                                            onChange={this.handleInputChange} />
+                                    </div>
                                 </div>
                             </div>
                             <span className="row">
-                                <a class="waves-effect waves-deep-purple btn-flat" style={{ color: 'white', fontWeight: 'bold', marginLeft: 15 }}>SEND</a>
+                                <a className="waves-effect waves-deep-purple btn-flat" style={{ color: 'black', fontWeight: 'bold', marginLeft: 15 }} onClick={() => this.submitForm()}>Hoàn thành</a>
                             </span>
                         </div>
                     </div>
                 </div>
             </div>
         );
+    }
+    submitForm = () => {
+        let tmpGbd = this.state.gbd.split(":");
+        let tgbd = moment(this.state.tgbd).unix() + parseInt(tmpGbd[0]) * 3600 + parseInt(tmpGbd[1]) * 60;
+        let tmpGkt = this.state.gkt.split(":");
+        let tgkt = moment(this.state.tgkt).unix() + parseInt(tmpGkt[0]) * 3600 + parseInt(tmpGkt[1]) * 60;
+        let body = {
+            "ten": this.state.tensk,
+            "tgbd": tgbd,
+            "tgkt": tgkt,
+            "diemCong": this.state.diemCong,
+            "thDu": 0
+        }
+        callApi('event', 'POST', body).then(res => {
+            if (res.data) {
+                window.M.toast({ html: 'Đã lên lịch sự kiện!' });
+                let tmpSk = this.state.dataEvents;
+                tmpSk.push(body);
+                this.setState({
+                    dataEvents: tmpSk
+                });
+            }
+        });
     }
 }
 
